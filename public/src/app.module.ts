fae2fc319776f854da0125c4ad1f0a190f965e5e
Marcin Wosinek <marcin.wosinek@gmail.com>
2021-12-21 17:02:56 +0000

    migrate home component

--- public/src/app.module.ts
+++ public/src/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule, DoBootstrap } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { UpgradeModule } from "@angular/upgrade/static";
-
 import moduleName from "./app.module.ajs";
+
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
   imports: [BrowserModule, UpgradeModule],
+  declarations: [HomeComponent],
+  entryComponents: [HomeComponent],
 })
 export class AppModule implements DoBootstrap {
   constructor(private upgrade: UpgradeModule) {}
